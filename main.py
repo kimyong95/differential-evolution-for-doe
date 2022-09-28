0059f753926786c0e953c612e31214c0e1983f77
Kim Yong <kimyong95@gmail.com>
2022-09-28 13:48:04 +0000

    fix code

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from scipy.optimize import rosen, differential_evolution, Bounds, LinearConstraint
 import numpy as np
+import cupy as np
 import matplotlib.pyplot as plt
 import signal
-# import cupy as cp
 
 design_factor = 7
-population = 100
-support_points = 100
+population = 10
+support_points = 10
 theta = np.asanyarray((-0.4926, -0.6280, -0.3283, 0.4378, 0.5283, -0.6120, -0.6837, -0.2061))
 y_plot = []
 
@@ -19,15 +19,16 @@ def save_plot(signum, frame):
 signal.signal(signal.SIGINT, save_plot)
 
 def obj_func(x: tuple, *args):
-    a = np.asanyarray(x) 
+    a = np.asanyarray(x)
     a = np.reshape(a, (support_points, 1+design_factor)) # p x (1+factor)
     split = np.hsplit(a, np.array([1, 1+design_factor]))
     n = split[0] # p x 1
     x = split[1] # p x factor
-    r = np.insert(x, 0, [1], 1) # p x (factor + 1)
+    z = np.ones((support_points,1))
+    r = np.append(z,x, 1) # p x (1 + factor)
 
     # (factor+1) x (factor+1)
-    information_matrix = np.sum([
+    s = [
         n[i].item() * \
         # 1/support_points * \
         np.exp(np.matmul(r_n, theta)) / (1+np.exp(np.matmul(r_n, theta)))**2 * \
@@ -36,23 +37,27 @@ def obj_func(x: tuple, *args):
             np.expand_dims(r_n, axis=0), # 1 x (factor+1)
         )
         for i, r_n in enumerate(r)
-    ], axis=0)
+    ]
+    s = np.asanyarray(s)
+    information_matrix = np.sum(s, axis=0)
 
     obj = -np.log(np.linalg.det(information_matrix))
+
     if np.isnan(obj):
         obj = np.inf
     return obj
 
 def termination_check(xk, convergence):
-    a = np.asanyarray(xk) 
+    a = np.asanyarray(xk)
     a = np.reshape(a, (support_points, 1+design_factor)) # p x (1+factor)
     split = np.hsplit(a, np.array([1, 1+design_factor]))
     n = split[0] # p x 1
     x = split[1] # p x factor
-    r = np.insert(x, 0, [1], 1) # p x (factor + 1)
+    z = np.ones((support_points,1))
+    r = np.append(z,x, 1) # p x (1 + factor)
     
     # (factor+1) x (factor+1)
-    information_matrix = np.sum([
+    s = [
         n[i].item() * \
         # 1/support_points * \
         np.exp(np.matmul(r_n, theta)) / (1+np.exp(np.matmul(r_n, theta)))**2 * \
@@ -61,7 +66,9 @@ def termination_check(xk, convergence):
             np.expand_dims(r_n, axis=0), # 1 x (factor+1)
         )
         for i, r_n in enumerate(r)
-    ], axis=0)
+    ]
+    s = np.asanyarray(s)
+    information_matrix = np.sum(s, axis=0)
 
     obj = -np.log(np.linalg.det(information_matrix))
     y_plot.append(-obj)
@@ -93,11 +100,12 @@ A = np.full((support_points, 1+design_factor), 0, dtype='i')
 A[:,0] = 1
 A = A.flatten()
 constrain = LinearConstraint(A, 1-0.05, 1+0.05) # probability sum to 1
-bounds = np.full((support_points, 1+design_factor), 0, dtype="f,f")
-bounds[:,:] = (-1,1) # design factor bounds
-bounds[:,0] = (0, 3/support_points) # replicates probability bounds
+bounds = np.full((support_points, 1+design_factor,2 ), 0, dtype="float")
+bounds[:,:,:] = np.array((-1,1)) # design factor bounds
+
+bounds[:,0,:] = np.array((0,2/support_points)) # replicates probability bounds
 bounds = bounds.flatten()
-bounds = bounds.tolist()
+bounds = bounds.reshape((-1,2))
 
 integrality = np.full((support_points, 1+design_factor), True, dtype='b')
 integrality[:,0] = False # observation probability are float
@@ -111,7 +119,7 @@ result = differential_evolution(
     callback=termination_check,
     strategy="rand2bin",
     popsize=population,
-    mutation=0.8,
+    mutation=0.6,
     disp=True,
     recombination=0.9,
     maxiter=20000,
